bf120962d9312682af3f94d1f96bb9e594aec06b
Sandeep Pradhan <paul.phoenix7285@gmail.com>
2021-12-28 23:17:54 +0530

    feat: added lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import PostPage from "./pages/PostPage";
-import UserPage from "./pages/UserPage";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const UserPage = lazy(() => import("./pages/UserPage"));
 
 function App() {
   return (
     <Router>
       <Header />
       <Container>
-        <Routes>
-          <Route path="/" element={<HomePage />} exact />
-          <Route path="/account/login" element={<LoginPage />} exact />
-          <Route path="/account/user" element={<UserPage />} exact />
-          <Route path="/account/post/:id" element={<PostPage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/account/login" element={<LoginPage />} exact />
+            <Route path="/account/user" element={<UserPage />} exact />
+            <Route path="/account/post/:id" element={<PostPage />} />
+          </Routes>
+        </Suspense>
       </Container>
     </Router>
   );
